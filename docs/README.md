d5d1d575289ec13de9f115a66d48e9e004290322
Salvydas Lukosius <sall@w-ss.io>
2022-04-07 07:15:12 +0100

    refactor(workspace): ⚡️ Update according to latest zsh plugin standard
    https://z.digitalclouds.dev/community/zsh_plugin_standard

--- docs/README.md
+++ docs/README.md
@@ -1,13 +1,6 @@
 <h1> ZI Annex Test </h1>
 
-## Introduction
-
-> **[?]**
-> This repository not compatible with previous versions (zplugin, zinit).
->
-> Please upgrade to [ZI](https://github.com/z-shell-zi)
-
-A [ZI](https://github.com/z-shell/zi) Annex (extension) that runs tests (via `make test`, for example) – if it finds any of them – after installing and updating
+A [ZI](https://github.com/z-shell/zi) annex (extension) that runs tests (via `make test`, for example) – if it finds any of them – after installing and updating
 a plugin or snippet. Simply load it like any other plugin to make it active:
 
 ```zsh
@@ -34,8 +27,12 @@ zi load …
 
 Example activation in the default quiet mode:
 
-![z-a-test activation](https://raw.githubusercontent.com/z-shell/z-a-test/main/images/z-p-test-1.png)
+![z-a-test activation](https://raw.githubusercontent.com/z-shell/z-a-test/main/docs/images/z-p-test-1.png)
 
 Example activation in non-quiet mode:
 
-![z-a-test activation](https://raw.githubusercontent.com/z-shell/z-a-test/main/images/z-p-test-2.png)
+![z-a-test activation](https://raw.githubusercontent.com/z-shell/z-a-test/main/docs/images/z-p-test-2.png)
+
+---
+
+This repository is compatible with [ZI](https://github.com/z-shell-zi)
--- z-a-test.plugin.zsh
+++ z-a-test.plugin.zsh
@@ -1,15 +1,30 @@
-# According to the Zsh Plugin Standard:
-# https://github.com/z-shell/zi/wiki/Zsh-Plugin-Standard
-0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+# -*- mode: zsh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
+# Standardized $0 Handling
+# https://z.digitalclouds.dev/community/zsh_plugin_standard#zero-handling
+0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-autoload -Uz za-test-handler za-test-make-targets
+# Functions directory
+# https://z.digitalclouds.dev/community/zsh_plugin_standard#funtions-directory
+if [[ $PMSPEC != *f* ]] {
+    fpath+=( "${0:h}/functions" )
+}
+
+# The Proposed Function-Name Prefixes
+# https://z.digitalclouds.dev/community/zsh_plugin_standard#the-proposed-function-name-prefixes
+autoload -Uz →za-test-handler →za-test-make-targets
 
 # An empty stub to fill the help handler fields
-za-test-help-null-handler() { :; }
+→za-test-help-null-handler() { :; }
 
 # Register atclone hook
-@zi-register-annex "z-a-test" hook:atclone-110 za-test-handler za-test-help-null-handler "notest|test''" # register a new ice-mod: test''
+@zi-register-annex "z-a-test" hook:atclone-110 \
+  →za-test-handler \
+  →za-test-help-null-handler "notest|test''" # register a new ice-mod: test''
 
 # Register atpull hook
-@zi-register-annex "z-a-test" hook:atpull-110 za-test-handler za-test-help-null-handler
+@zi-register-annex "z-a-test" hook:atpull-110 \
+  →za-test-handler \
+  →za-test-help-null-handler
+
+# vim: ft=zsh sw=2 ts=2 et
