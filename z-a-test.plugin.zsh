b9d2c9631acacaa87d0ace071bdda01b2b13bf87
Salvydas Lukosius <sall@w-ss.io>
2022-10-30 14:47:36 +0000

    Update z-a-test.plugin.zsh
    Signed-off-by: Salvydas Lukosius <sall@w-ss.io>

--- z-a-test.plugin.zsh
+++ z-a-test.plugin.zsh
@@ -1,17 +1,18 @@
 # -*- mode: zsh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
+#
 # Standardized $0 Handling
 # https://z.digitalclouds.dev/community/zsh_plugin_standard#zero-handling
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
 # Functions directory
-# https://z.digitalclouds.dev/community/zsh_plugin_standard#funtions-directory
+# https://wiki.zshell.dev/community/zsh_plugin_standard#funtions-directory
 if [[ $PMSPEC != *f* ]] {
     fpath+=( "${0:h}/functions" )
 }
 
 # The Proposed Function-Name Prefixes
-# https://z.digitalclouds.dev/community/zsh_plugin_standard#the-proposed-function-name-prefixes
+# https://wiki.zshell.dev/community/zsh_plugin_standard#the-proposed-function-name-prefixes
 autoload -Uz →za-test-handler →za-test-make-targets
 
 # An empty stub to fill the help handler fields
